dbcd5a83b9d6c5ca557c88e55019d8dbb2ad2ee1
GDkikuchi <gamedo.kikuchi.sgc@gmail.com>
2024-10-13 03:14:41 +0900

    セーブデータ基盤追加

--- TroubleShooting/Assets/Program/Script/ScoreManager.cs
+++ TroubleShooting/Assets/Program/Script/ScoreManager.cs
@@ -1,14 +1,27 @@
+using DG.Tweening.Plugins.Core.PathCore;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using static ScoreManager;
 
 public class ScoreManager : BaseManager
 {
+    //改行コード
+    private const string SPRIT_TEXT = "\n\r";
+
+    //セーブフォルダ名
+    private const string FOLDER_NAME = "SaveData.json";
+
     /// <summary> 現在のスコア</summary>
     private int m_currentScore;
 
     /// <summary>すべてのスコア</summary>
-    private List<ScoreData> m_allScore;
+    [SerializeField]
+    public List<ScoreData> m_allScore;
+
+    /// <summary> 保存ファイルパス</summary>
+    private string m_saveFilePath;
 
     /// <summary>
     /// スコアデータ
@@ -23,6 +36,8 @@ public class ScoreManager : BaseManager
     {
         DontDestroyOnLoad(this);
         m_allScore = new List<ScoreData>(100);
+        m_saveFilePath = Application.persistentDataPath + "/" + FOLDER_NAME;
+        Debug.Log(m_saveFilePath);
     }
 
     /// <summary>
@@ -31,7 +46,14 @@ public class ScoreManager : BaseManager
     /// <param name="_addScore"></param>
     public void SetScore( int _addScore )
     {
-        m_currentScore += _addScore;
+        m_currentScore = _addScore;
+    }
+
+    /// <summary>スコア取得</summary>
+    /// <returns>現在のスコア</returns>
+    public int GetScore() 
+    {
+        return m_currentScore;
     }
 
     /// <summary>
@@ -39,14 +61,66 @@ public class ScoreManager : BaseManager
     /// </summary>
     /// <param name="score"></param>
     /// <param name="useName"></param>
-    public void AddScoreData( int score, string useName ) 
+    public void AddScoreData( int _score ) 
     {
         ScoreData data = new ScoreData();
 
-        data.score = score;
-        data.userName = useName;
+        data.score = _score;
+        //data.userName = _useName;
         m_allScore.Add(data);
+        SaveScore(data);
     }
 
+    /// <summary>
+    /// ランキングスコア読み込み
+    /// </summary>
+    public void LoadSaveData()
+    {
+        if ( File.Exists( m_saveFilePath ) )
+        {
+            StreamReader reader = new StreamReader(m_saveFilePath);
+            string scoreData = reader.ReadToEnd();
+            // 改行毎に切り分け
+            string[] rankingList = scoreData.Split(SPRIT_TEXT);
+            foreach ( string ranking in rankingList ) 
+            {
+                m_allScore.Add(JsonUtility.FromJson<ScoreData>(ranking));
+            }
+
+            reader.Close();
+        }
+    }
+
+    /// <summary>
+    /// スコア追加保存
+    /// </summary>
+    /// <param name="_data"></param>
+    public void SaveScore(ScoreData _data)
+    {
+        string scoreData = "";
+        foreach ( ScoreData data in m_allScore ) 
+        {
+            scoreData += JsonUtility.ToJson(data);
+            scoreData += SPRIT_TEXT;
+
+        }
+        scoreData += JsonUtility.ToJson(_data);
+        scoreData += SPRIT_TEXT;
+
+        StreamWriter writer = new StreamWriter(m_saveFilePath, false);
+        writer.WriteLine(scoreData);
+        writer.Close();
+    }
+
+    /// <summary>
+    /// スコア全消去
+    /// </summary>
+    /// <param name="_data"></param>
+    public void DestorySaveData()
+    {
+        StreamWriter writer = new StreamWriter(m_saveFilePath, false);
+        writer.WriteLine("");
+        writer.Close();
+    }
 
 }
