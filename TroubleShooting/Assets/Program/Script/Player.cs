2945b094f0dd44930101a96fa43eafb632a4c482
S-JOBI23-055\student <231105@st.yoshida-g.ac.jp>
2024-10-12 17:09:49 +0900

    Unity内にDOTWeenをインポート。 プレイヤーオブジェクトの発射。成否判定の実装。

---
+++ TroubleShooting/Assets/Program/Script/Player.cs
@@ -0,0 +1,108 @@
+//---------------------------------------------------------------
+//
+// プレイヤー処理 [ Player.cs ]
+// Author:Kenta Nakamoto
+// Data:2024/10/12
+// Update:2024/10/12
+//
+//---------------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Player : MonoBehaviour
+{
+    //--------------------------
+    // フィールド
+
+    /// <summary>
+    /// 操作フラグ
+    /// </summary>
+    private bool moveFlag = false;
+
+    /// <summary>
+    /// プレイヤー生成スクリプト
+    /// </summary>
+    private PlayerGenerate playerGenerate;
+
+    /// <summary>
+    /// 移動秒数
+    /// </summary>
+    [SerializeField] private float moveSecond = 0f;
+
+    //--------------------------
+    // メソッド
+
+    /// <summary>
+    /// 初期処理
+    /// </summary>
+    private void Start()
+    {
+        playerGenerate = GetComponentInParent<PlayerGenerate>();
+    }
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    private void Update()
+    {
+        if (moveFlag) return;
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {   // W・↑キー操作
+            moveFlag = true;
+
+            // 移動処理
+            this.transform.DOMove(new Vector3(0f, 5f, 0f), moveSecond);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {   // S・↓キー操作
+            moveFlag = true;
+
+            // 移動処理
+            this.transform.DOMove(new Vector3(0f, -5f, 0f), moveSecond);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {   // A・←キー操作
+            moveFlag = true;
+
+            // 移動処理
+            this.transform.DOMove(new Vector3(-5f, 0f, 0f), moveSecond);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {   // D・→キー操作
+            moveFlag = true;
+
+            // 移動処理
+            this.transform.DOMove(new Vector3(5f, 0f, 0f), moveSecond);
+        }
+    }
+
+    /// <summary>
+    /// 衝突したときの処理
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == this.gameObject.tag)
+        {   // 成功時
+            playerGenerate.GeneratePlayer();
+            Destroy(this.gameObject);
+        }
+        else
+        {   // 失敗時
+            Invoke("Failure", 2.0f);
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    /// <summary>
+    /// 失敗時の処理
+    /// </summary>
+    private void Failure()
+    {
+        playerGenerate.GeneratePlayer();
+        Destroy(this.gameObject);
+    }
+}
---
+++ TroubleShooting/Assets/Program/Script/PlayerGenerate.cs
@@ -0,0 +1,52 @@
+//---------------------------------------------------------------
+//
+// プレイヤー生成処理 [ PlayerGenerate.cs ]
+// Author:Kenta Nakamoto
+// Data:2024/10/12
+// Update:2024/10/12
+//
+//---------------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class PlayerGenerate : MonoBehaviour
+{
+    //--------------------------
+    // フィールド
+
+    /// <summary>
+    /// プレイヤープレハブ
+    /// </summary>
+    [SerializeField] private List<GameObject> playerObjs;
+
+    //--------------------------
+    // メソッド
+
+    /// <summary>
+    /// 初期処理
+    /// </summary>
+    void Start()
+    {
+        // プレイヤーの生成
+        Instantiate(playerObjs[Random.Range(0, playerObjs.Count)], Vector3.zero, Quaternion.identity,this.gameObject.transform);
+    }
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// プレイヤー生成処理
+    /// </summary>
+    public void GeneratePlayer()
+    {
+        // プレイヤーの生成
+        Instantiate(playerObjs[Random.Range(0, playerObjs.Count)], Vector3.zero, Quaternion.identity,this.gameObject.transform);
+    }
+}
