20531cc1ce492d4eeb96ee2fba2b08b808f9a2dc
szkkit <32424014+szkkit@users.noreply.github.com>
2024-10-13 10:05:12 +0900

    SEの差分

--- TroubleShooting/Assets/AudioManager_KanKikuchi/Scripts/SEPath.cs
+++ TroubleShooting/Assets/AudioManager_KanKikuchi/Scripts/SEPath.cs
@@ -5,6 +5,30 @@
 /// </summary>
 public static class SEPath{
 
+	public const string APPEAR_00    = "SE/appear_00";
+	public const string APPEAR_01    = "SE/appear_01";
+	public const string DECIDE       = "SE/decide";
+	public const string DISAPPEAR_00 = "SE/disappear_00";
+	public const string DISAPPEAR_01 = "SE/disappear_01";
+	public const string DISAPPEAR_02 = "SE/disappear_02";
+	public const string END          = "SE/End";
+	public const string OTHER_00     = "SE/other_00";
+	public const string OTHER_01     = "SE/other_01";
+	public const string OTHER_02     = "SE/other_02";
+	public const string OTHER_03     = "SE/other_03";
+	public const string OTHER_04     = "SE/other_04";
+	public const string OTHER_05     = "SE/other_05";
+	public const string PUYO         = "SE/puyo";
+	public const string SHOT_00      = "SE/shot_00";
+	public const string SHOT_01      = "SE/shot_01";
+	public const string SHOT_02      = "SE/shot_02";
+	public const string SHOT_03      = "SE/shot_03";
+	public const string SHOT_04      = "SE/shot_04";
+	public const string SHOT_05      = "SE/shot_05";
+	public const string START_00     = "SE/Start_00";
+	public const string START_01     = "SE/Start_01";
+	public const string START_02     = "SE/Start_02";
+	public const string WARNING      = "SE/warning";
 
 }
 
